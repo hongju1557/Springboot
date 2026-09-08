100da4addc19f4ca552b73030d625d13c3b9b6bf
hongju1557 <fuyuka666@gmail.com>
2026-09-08 09:36:58 +0900

    0906

---
+++ test1453/src/test/java/com/testtest/test1453/Test1453ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtest.test1453;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1453ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
