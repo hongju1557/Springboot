0b11aafe96c92215770cd4c74c6aa0558440275c
hongju1557 <fuyuka666@gmail.com>
2026-09-10 12:58:49 +0900

    0910

--- test/src/main/java/com/biz/test/dto/EmpSearchCond.java
+++ test/src/main/java/com/biz/test/dto/EmpSearchCond.java
@@ -2,12 +2,21 @@ package com.biz.test.dto;
 
 public class EmpSearchCond {
     private String name;
+    private String deptId;
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
     }
 }
